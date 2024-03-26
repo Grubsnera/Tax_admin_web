b343be754eeebeb459c7471d87a3cc8d37048c7c
Albert <44517335+Grubsnera@users.noreply.github.com7>
2024-03-26 12:46:18 +0200

    Town: Python script to display tabulizer lists.

---
+++ sql/Tabulizer scripts/Town/tabulizer_town.sql
@@ -0,0 +1,21 @@
+﻿Select
+    c.country_name,
+    t.town_name,
+    t.town_suburb,
+    t.town_dialcode,
+    t.town_postcode,
+    t.town_coordinates,
+    Concat('<a href = "index.php?option=com_rsform&formId=', t.form_edit_id, '&id=0', '&action=add">Add</a>', ' | ',
+    '<a href = "index.php?option=com_rsform&formId=', t.form_edit_id, '&id=', t.town_id, '&action=edit">Edit</a>',
+    ' | ', '<a href = "index.php?option=com_rsform&formId=', t.form_edit_id, '&id=', t.town_id,
+    '&action=copy">Copy</a>') As actions
+From
+    sys_town t Inner Join
+    sys_country c On c.country_iso2 = t.country_iso2
+Where
+    -- t.town_name like ('%{user_param_1:cmd}%')  
+    t.town_name like ('%potch%')
+Group By
+    c.country_name,
+    t.town_name,
+    t.town_suburb
