995e6f6558203045441ad6491f0aada6b8fb6a49
Albert <44517335+Grubsnera@users.noreply.github.com7>
2024-03-26 12:43:37 +0200

    Town: RSForm: Form setup scripts.

---
+++ sql/RSForm scripts/08 Town/php scripts - form display.php
@@ -0,0 +1,20 @@
+<?php
+
+// Display different heading and button depending on the action
+$action = JFactory::getApplication()->input->getString('action');
+$description = 'town';
+if ($action == 'delete') {
+	$formLayout = str_replace('{header_text_message}', 'DELETE a '.$description.'!', $formLayout);
+	$formLayout = str_replace('{save_button_text}', 'Delete', $formLayout);
+} elseif ($action == 'edit') {
+	$formLayout = str_replace('{header_text_message}', 'Edit or change a '.$description.'!', $formLayout);
+	$formLayout = str_replace('{save_button_text}', 'Save', $formLayout);
+} elseif ($action == 'copy') {
+	$formLayout = str_replace('{header_text_message}', 'Make a copy of an existing '.$description.'!', $formLayout);
+	$formLayout = str_replace('{save_button_text}', 'Copy', $formLayout);
+} elseif ($action == 'add') {
+	$formLayout = str_replace('{header_text_message}', 'Add a new '.$description.'!', $formLayout);
+	$formLayout = str_replace('{save_button_text}', 'Add', $formLayout);
+}
+
+?>
