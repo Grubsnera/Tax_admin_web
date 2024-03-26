6a106e7b71bcc512316e2cb6ef71eadaee0b9755
Albert <44517335+Grubsnera@users.noreply.github.com7>
2024-03-26 12:43:02 +0200

    Country: RSForm: Form setup scripts.

---
+++ sql/RSForm scripts/07 Country/php scripts - form has been processed.php
@@ -0,0 +1,22 @@
+<?php
+
+// Choose the mapping profile depending on the action
+if ($_POST['form']['action'] == 'add') {
+    unset($mappings[1]);
+    unset($mappings[2]);
+} elseif ($_POST['form']['action'] == 'edit') {
+    unset($mappings[0]);
+    unset($mappings[2]);
+} elseif ($_POST['form']['action'] == 'copy') {
+    unset($mappings[1]);
+    unset($mappings[2]);
+} elseif ($_POST['form']['action'] == 'delete') {
+    unset($mappings[0]);
+    unset($mappings[1]);
+} else {
+    unset($mappings[0]);
+    unset($mappings[1]);
+    unset($mappings[2]);
+}
+
+?>
